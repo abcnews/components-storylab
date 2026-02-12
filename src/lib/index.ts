7ef10de5545da1ab59ec18f6438603365ffd8ac5
Simon Elvery <simon@elvery.net>
2026-02-12 13:35:49 +1000

    Clean up exports - make them available at the top level - add sourcemaps support

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
-// Reexport your entry components here
+// This file only re-exports individual components, functions and types
+// It shouldn't have any functionality of its own.
 
-// iframes
 export { default as initDarkModeIframe } from "./utils/initDarkModeIframe/initDarkModeIframe.ts";
+export { TypographyProvider } from "./typography/index.ts"
+export { MapLibreLoader, utils as mapLibreUtils, type maplibregl } from './mapLibre/index.ts'
